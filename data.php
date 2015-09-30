1009d136c567632f14404cc6434f611051f4aef7
Reio raudheiding <reiorau@greeny.cs.tlu.ee>
2015-09-30 12:00:56 +0300

    pmst valmis

--- data.php
+++ data.php
@@ -1,5 +1,24 @@
 <?php
+	require_once("functions.php");
 	//data.php
 	// siia pääseb ligi sisseloginud kasutaja
-
+	//kui kasutaja ei ole sisseloginud,
+	//sunnab ta data.php lehele
+		if(!isset($_SESSION["logged_in_user_id"])){
+		header("Location: data.php");
+	}
+	
+	//kasutaja tahab välja logida
+	if(isset($_GET["logout"])) {
+		//aadressireal on olemas muutuja logout
+		
+		//kustutame kõik sessioon muutujad ja peatame sessiooni
+		session_destroy();
+		
+		header("Location: login.php");
+	}
+	
 ?>
+<p>
+	Jou, <?=$_SESSION["logged_in_user_email"];?>
+	<a href="?logout=1"> Logi välja <a>
--- functions.php
+++ functions.php
@@ -1,5 +1,53 @@
 <?php 
-	// functions.php
-	// siia tulevad funktsioonid, kõik mis seotud AB'iga
+	
+	// Loon AB'i ühenduse
+	require_once("../config_global.php");
+	$database = "if15_reiorau";
+	
+	//tekitatakse session, mida hoitakse serveris
+	//kõik session muutujad on kättesaadavad kuni browseri aken pannakse kinni
+	session_start();
+	
+	//Võtab andmed ja sisestab ab'i
+	function createUser($create_email, $hash) {
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+		$stmt->bind_param("ss", $create_email, $hash);
+		$stmt->execute();
+		$stmt->close();
+		
+		$mysqli->close();
+	}
+	
+	function loginUser($email, $hash) {
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+		$stmt->bind_param("ss", $email, $hash);
+		$stmt->bind_result($id_from_db, $email_from_db);
+		$stmt->execute();
+		if($stmt->fetch()){
+			// ab'i oli midagi
+			echo "Email ja parool õiged, kasutaja id=".$id_from_db;
+			
+			// tekitan sessiooni muutujad
+			$_SESSION["logged_in_user_id"] = $id_from_db;
+			$_SESSION["logged_in_user_email"] = $email_from_db;
+			
+			//suunan data.php lehele
+			header("Location: data.php");
+		}else{
+			// ei leidnud
+			echo "Wrong credentials!";
+		}
+		$stmt->close();
+		
+		$mysqli->close();
+	}
+	
+	// fn sample
+	function hello($name, $age){
+		echo $name." ".$age;
+	}
+	
 
 ?>
